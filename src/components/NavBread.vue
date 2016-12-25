02da3ed18d2eee963f9f21a81e8e3d108b79ff03
wangpeng <597084762@qq.com>
2016-12-25 19:42:09 +0800

    11.1课程

---
+++ src/components/NavBread.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+   <div class="nav-breadcrumb-wrap">
+     <div class="container">
+       <nav class="nav-breadcrumb">
+         <a href="/">主页</a>
+         <span>商品列表</span>
+       </nav>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
