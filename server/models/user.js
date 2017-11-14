53897e3d595cc6316dccc827452a44242b84682d
wangpeng <597084762@qq.com>
2017-11-14 22:20:42 +0800

    加入购物车功能

---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+/**
+ * Created by wangpeng on 2017/11/14.
+ */
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+
+var produtSchema = new Schema({
+  "userId":String,
+  "userName":String,
+  "userPwd":String,
+  "orderList":Array,
+  "cartList":[
+    {
+      "productId":String,
+      "productName":String,
+      "salePrice":String,
+      "productImage":String,
+      "checked":String,
+      "productNum":String
+    }
+  ],
+  "addressList":[
+    {
+      "addressId": String,
+      "userName": String,
+      "streetName": String,
+      "postCode": Number,
+      "tel": Number,
+      "isDefault": Boolean
+    }
+  ]
+})
+
+module.exports = mongoose.model('User', produtSchema);
