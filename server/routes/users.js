9715aaae7e11796c43b1f0af7a87a640282f91c8
wangpeng <597084762@qq.com>
2017-11-22 17:43:14 +0800

    订单生成页面前后端实现

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('./../models/user');
+require('./../util/util');
 
 router.post('/login', function (req, res, next) {
   var param = {
@@ -11,20 +12,20 @@ router.post('/login', function (req, res, next) {
     userPwd: req.body.userPwd
   };
   User.findOne(param, function (err, doc) {
-    if(err){
+    if (err) {
       res.json({
         status: '1',
         msg: err.message
       })
-    }else{
-      if(doc){
+    } else {
+      if (doc) {
         res.cookie('userId', doc.userId, {
           path: '/',
-          maxAge: 1000*60*60
+          maxAge: 1000 * 60 * 60
         });
         res.cookie('userName', doc.userName, {
           path: '/',
-          maxAge: 1000*60*60
+          maxAge: 1000 * 60 * 60
         });
         res.json({
           status: '0',
@@ -38,26 +39,26 @@ router.post('/login', function (req, res, next) {
   })
 })
 
-router.post("/logout", function (req,res,next) {
-  res.cookie("userId","",{
-    path:"/",
-    maxAge:-1
+router.post("/logout", function (req, res, next) {
+  res.cookie("userId", "", {
+    path: "/",
+    maxAge: -1
   });
   res.json({
-    status:"0",
-    msg:'',
-    result:''
+    status: "0",
+    msg: '',
+    result: ''
   })
 })
 
 router.get('/checkLogin', function (req, res, next) {
-  if(req.cookies.userId){
+  if (req.cookies.userId) {
     res.json({
       status: '0',
       msg: '',
       result: req.cookies.userName || ''
     })
-  }else {
+  } else {
     res.json({
       status: '1',
       msg: '未登录',
@@ -66,21 +67,21 @@ router.get('/checkLogin', function (req, res, next) {
   }
 })
 
-router.get("/cartList", function (req,res,next) {
+router.get("/cartList", function (req, res, next) {
   var userId = req.cookies.userId;
-  User.findOne({userId:userId}, function (err,doc) {
-    if(err){
+  User.findOne({userId: userId}, function (err, doc) {
+    if (err) {
       res.json({
-        status:'1',
-        msg:err.message,
-        result:''
+        status: '1',
+        msg: err.message,
+        result: ''
       });
-    }else{
-      if(doc){
+    } else {
+      if (doc) {
         res.json({
-          status:'0',
-          msg:'',
-          result:doc.cartList
+          status: '0',
+          msg: '',
+          result: doc.cartList
         });
       }
     }
@@ -88,86 +89,86 @@ router.get("/cartList", function (req,res,next) {
 });
 
 //购物车删除
-router.post("/cartDel", function (req,res,next) {
-  var userId = req.cookies.userId,productId = req.body.productId;
+router.post("/cartDel", function (req, res, next) {
+  var userId = req.cookies.userId, productId = req.body.productId;
   User.update({
-    userId:userId
-  },{
-    $pull:{
-      'cartList':{
-        'productId':productId
+    userId: userId
+  }, {
+    $pull: {
+      'cartList': {
+        'productId': productId
       }
     }
-  }, function (err,doc) {
-    if(err){
+  }, function (err, doc) {
+    if (err) {
       res.json({
-        status:'1',
-        msg:err.message,
-        result:''
+        status: '1',
+        msg: err.message,
+        result: ''
       });
-    }else{
+    } else {
       res.json({
-        status:'0',
-        msg:'',
-        result:'suc'
+        status: '0',
+        msg: '',
+        result: 'suc'
       });
     }
   });
 })
 //修改商品数量
-router.post("/cartEdit", function (req,res,next) {
+router.post("/cartEdit", function (req, res, next) {
   var userId = req.cookies.userId,
     productId = req.body.productId,
     productNum = req.body.productNum,
     checked = req.body.checked;
-  User.update({"userId":userId,"cartList.productId":productId},{
-    "cartList.$.productNum":productNum,
-    "cartList.$.checked":checked,
-  }, function (err,doc) {
-    if(err){
+  User.update({"userId": userId, "cartList.productId": productId}, {
+    "cartList.$.productNum": productNum,
+    "cartList.$.checked": checked,
+  }, function (err, doc) {
+    if (err) {
       res.json({
-        status:'1',
-        msg:err.message,
-        result:''
+        status: '1',
+        msg: err.message,
+        result: ''
       });
-    }else{
+    } else {
       res.json({
-        status:'0',
-        msg:'',
-        result:'suc'
+        status: '0',
+        msg: '',
+        result: 'suc'
       });
     }
   })
 })
 
 //全部选中接口
-router.post("/editCheckAll", function (req,res,next) {
+router.post("/editCheckAll", function (req, res, next) {
   var userId = req.cookies.userId,
-    checkAll = req.body.checkAll?'1':'0';
-  User.findOne({userId:userId}, function (err,user) {
-    if(err){
+    checkAll = req.body.checkAll ? '1' : '0';
+  User.findOne({userId: userId}, function (err, user) {
+    if (err) {
       res.json({
-        status:'1',
-        msg:err.message,
-        result:''
+        status: '1',
+        msg: err.message,
+        result: ''
       });
-    }else{
-      if(user){
-        user.cartList.forEach((item)=>{
+    } else {
+      if (user) {
+        user.cartList.forEach((item) => {
           item.checked = checkAll;
         })
-        user.save(function (err1,doc) {
-          if(err1){
+        user.save(function (err1, doc) {
+          if (err1) {
             res.json({
-              status:'1',
-              msg:err1,message,
-              result:''
+              status: '1',
+              msg: err1, message,
+              result: ''
             });
-          }else{
+          } else {
             res.json({
-              status:'0',
-              msg:'',
-              result:'suc'
+              status: '0',
+              msg: '',
+              result: 'suc'
             });
           }
         })
@@ -177,65 +178,65 @@ router.post("/editCheckAll", function (req,res,next) {
 });
 
 //查询用户地址接口
-  router.get("/addressList", function (req,res,next) {
-      var userId = req.cookies.userId;
-      User.findOne({userId:userId}, function (err,doc) {
-          if(err){
-              res.json({
-                  status:'1',
-                  msg:err.message,
-                  result:''
-              });
-            }else{
-              res.json({
-                  status:'0',
-                  msg:'',
-                  result:doc.addressList
-              });
-            }
-        })
-    });
+router.get("/addressList", function (req, res, next) {
+  var userId = req.cookies.userId;
+  User.findOne({userId: userId}, function (err, doc) {
+    if (err) {
+      res.json({
+        status: '1',
+        msg: err.message,
+        result: ''
+      });
+    } else {
+      res.json({
+        status: '0',
+        msg: '',
+        result: doc.addressList
+      });
+    }
+  })
+});
 
 //设置默认地址接口
-router.post("/setDefault", function (req,res,next) {
+router.post("/setDefault", function (req, res, next) {
   var userId = req.cookies.userId,
     addressId = req.body.addressId;
-  if(!addressId){
+  if (!addressId) {
     res.json({
-      status:'1003',
-      msg:'addressId is null',
-      result:''
+      status: '1003',
+      msg: 'addressId is null',
+      result: ''
     });
-  }else{
-    User.findOne({userId:userId}, function (err,doc) {
-      if(err){
+  } else {
+    User.findOne({userId: userId}, function (err, doc) {
+      if (err) {
         res.json({
-          status:'1',
-          msg:err.message,
-          result:''
+          status: '1',
+          msg: err.message,
+          result: ''
         });
-      }else{
+      } else {
         var addressList = doc.addressList;
-        addressList.forEach((item)=>{
-          if(item.addressId ==addressId){
+        addressList.forEach((item) => {
+          if (item.addressId == addressId) {
             item.isDefault = true;
-          }else{
+          } else {
             item.isDefault = false;
           }
         });
 
-        doc.save(function (err1,doc1) {
-          if(err){
+        doc.save(function (err1, doc1) {
+          if (err) {
             res.json({
-              status:'1',
-              msg:err.message,
-              result:''
+              status: '1',
+              msg: err.message,
+              result: ''
             });
-          }else{
+          } else {
             res.json({
-              status:'0',
-              msg:'',
-              result:''
+              status: '0',
+              msg: '',
+              result: ''
             });
           }
         })
@@ -245,49 +246,117 @@ router.post("/setDefault", function (req,res,next) {
 });
 
 //删除地址接口
-router.post("/delAddress", function (req,res,next) {
-  var userId = req.cookies.userId,addressId = req.body.addressId;
-  User.findOne({userId:userId}, function (err,doc) {
-    if(err){
+router.post("/delAddress", function (req, res, next) {
+  var userId = req.cookies.userId, addressId = req.body.addressId;
+  User.findOne({userId: userId}, function (err, doc) {
+    if (err) {
       res.json({
-        status:'1',
-        msg:err.message,
-        result:''
+        status: '1',
+        msg: err.message,
+        result: ''
       });
-    }else{
-      if(doc.addressList.length > 1){
+    } else {
+      if (doc.addressList.length > 1) {
         User.update({
-          userId:userId
-        },{
-          $pull:{
-            'addressList':{
-              'addressId':addressId
+          userId: userId
+        }, {
+          $pull: {
+            'addressList': {
+              'addressId': addressId
             }
           }
-        }, function (err,doc) {
-          if(err){
+        }, function (err, doc) {
+          if (err) {
             res.json({
-              status:'1',
-              msg:err.message,
-              result:''
+              status: '1',
+              msg: err.message,
+              result: ''
             });
-          }else{
+          } else {
             res.json({
-              status:'0',
-              msg:'',
-              result:''
+              status: '0',
+              msg: '',
+              result: ''
             });
           }
         });
       } else {
         res.json({
-          status:'1',
-          msg:'至少保留一条收货地址！',
-          result:''
+          status: '1',
+          msg: '至少保留一条收货地址！',
+          result: ''
         });
       }
     }
   })
 
 });
+
+// 创建订单接口
+router.post("/payMent", function (req, res, next) {
+  var userId = req.cookies.userId,
+    addressId = req.body.addressId,
+    orderTotal = req.body.orderTotal;
+  User.findOne({userId: userId}, function (err, doc) {
+    if (err) {
+      res.json({
+        status: "1",
+        msg: err.message,
+        result: ''
+      });
+    } else {
+      var address = '', goodsList = [];
+      //获取当前用户的地址信息
+      doc.addressList.forEach((item) => {
+        if (addressId == item.addressId) {
+          address = item;
+        }
+      })
+      //获取用户购物车的购买商品
+      doc.cartList.filter((item) => {
+        if (item.checked == '1') {
+          goodsList.push(item);
+        }
+      });
+
+      var platform = '622';
+      var r1 = Math.floor(Math.random() * 10);
+      var r2 = Math.floor(Math.random() * 10);
+
+      var sysDate = new Date().Format('yyyyMMddhhmmss');
+      var createDate = new Date().Format('yyyy-MM-dd hh:mm:ss');
+      var orderId = platform + r1 + sysDate + r2;
+      var order = {
+        orderId: orderId,
+        orderTotal: orderTotal,
+        addressInfo: address,
+        goodsList: goodsList,
+        orderStatus: '1',
+        createDate: createDate
+      };
+
+      doc.orderList.push(order);
+
+      doc.save(function (err1, doc1) {
+        if (err1) {
+          res.json({
+            status: "1",
+            msg: err.message,
+            result: ''
+          });
+        } else {
+          res.json({
+            status: "0",
+            msg: '',
+            result: {
+              orderId: order.orderId,
+              orderTotal: order.orderTotal
+            }
+          });
+        }
+      });
+    }
+  })
+});
+
 module.exports = router;
